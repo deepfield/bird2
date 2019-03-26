a67cc13aaebd4a6b28fe89a2a06be8baca7cb3da
Peter Winkler <peter.1.winkler@nokia.com>
2019-03-26 16:48:02 -0400

    WIP

--- build-bird-2.0.4-1.df.sh
+++ build-bird-2.0.4-1.df.sh
@@ -2,46 +2,47 @@
 #
 # deepfield provided shell script to build a bird debian
 #
-# build - prerequisites:
+# build - prerequisites are documented in debian/control under build-depends
 #
-# set up environment
-# apt-get install -y build-essential autoconf flex bison libncurses5-dev git libreadline-dev \
-#    debmake
-#
-# git clone --depth 1 -b ${GIT_BRANCH} ${GIT_REPO}
-# cd $SOURCE_DIR
-# VERSION=$(cat VERSION)
-#
+
+PKG_NAME=bird2
+PKG_VERSION=2.0.4
+PKG_REV=1.df
+
+VERSION=${PKG_VERSION}-${PKG_REV}
+
+GIT_REPO=https://github.com/deepfield/bird2.git
+GIT_BRANCH=2.0.4
 
 CUR_DIR=${PWD}
 MAINTAINER=deepfield-syseng
 BUILD_ROOT_DIR=/tmp
-BUILD_DIR=bird2-$(date +'%s')
-GIT_REPO=https://github.com/deepfield/bird2.git
-GIT_BRANCH=2.0.4
-VERSION=2.0.4-1.df
+BUILD_DIR=${PKG_NAME}-$(date +'%s')/${PKG_NAME}-${VERSION}
 
 mkdir -p ${BUILD_ROOT_DIR}/${BUILD_DIR}
 cd ${BUILD_ROOT_DIR}
 
-git clone ${GIT_REPO} ${BUILD_DIR}
+git clone -b ${GIT_BRANCH} ${GIT_REPO} ${BUILD_DIR}
+
 cd ${BUILD_DIR}
 
 git clean -dxf
 
+# DEV only
+# cp -r ${CUR_DIR}/debian/ .
+
 # build the actual debian
 autoreconf
 # these are the default options, but let's state them specifically
-./configure --enable-client --enable-pthreads --enable-memcheck "--with-protocols=bfd babel bgp mrt ospf perf pipe radv rip static" \
+./configure --enable-client --enable-pthreads --enable-memcheck \
+    "--with-protocols=bfd babel bgp mrt ospf perf pipe radv rip static" \
     --enable-mpls-kernel --with-iproutedir=/etc/iproute2
 
-dpkg-buildpackage -rfakeroot
-
-mv *.deb /tmp
-
-echo "Debian files have been saved in /tmp"
-
+ls -ahl debian
+dpkg-buildpackage -b -us -uc
 cd ..
+mv *.deb ${CUR_DIR}
+echo "Debian files have been saved in ${CUR_DIR}"
 
 # Clean up
-rm -rf $(BUILD_DIR)/bird2
+rm -rf ${BUILD_DIR}
