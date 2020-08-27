69f08be4293a254fed10163ed0cf3d5ee1f124b1
Peter Winkler <peter.1.winkler@nokia.com>
2020-08-27 08:49:12 -0400

    QP-2587 - build process and .vscode

--- tools/df/build/build-bird-2.0.4-6.df.sh
+++ tools/df/build/build-bird-2.0.4-6.df.sh
@@ -9,8 +9,9 @@
 BUILD_SRC=git
 BUILD_CHECK_DEP=YES
 # argument handling 
-# ./build-bird2-2.0.4-4-df.sh local   - will build local
-# ./build-bird2-2.0.4-4-df.sh tag 2.0.4-3 - will build 2.0.4-3 tag
+# ./build-bird2-2.0.4-6-df.sh local   - will build local
+# ./build-bird2-2.0.4-6-df.sh tag 2.0.4-3 - will build 2.0.4-3 tag
+GIT_TAG=2.0.4-6.df
 
 while (( "$#" )); do
         case "$1" in
@@ -57,7 +58,7 @@ fi
 
 PKG_NAME=bird2
 PKG_VERSION=2.0.4
-PKG_REV=4.df
+PKG_REV=6.df
 PKG_ARCH=amd64
 
 VERSION=${PKG_VERSION}-${PKG_REV}
